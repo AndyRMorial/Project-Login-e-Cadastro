16c182d32f3f53630e764c3c0c5b7a2373b7f455
AndyRMorial <andrew.morial@gmail.com>
2023-11-29 18:33:16 -0300

    Login

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hello from './components/Hello'
+import CustomerForm from './components/CustomerForm'
+
+import { CustomerProvider } from './contexts/customer'
+
+import './App.css'
+
+function App() {  
+    return(
+    <div className='app'>
+      <CustomerProvider>
+        <Hello/>
+        <CustomerForm/>
+      </CustomerProvider>
+    </div>
+  )
+}
+
+export default App
